6f2af84e5631c47986ede920b98171c0ff1c3bcc
Akbarjon Joraqulov <akbarjon0445554@mail.ru>
2024-02-05 19:39:15 +0300

    Add vich-uploader bundle

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -3,12 +3,29 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\BookRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Get(),
+        new Delete()
+    ],
+    normalizationContext: ['groups' => ['book:read']],
+    denormalizationContext: ['groups' => ['book:write']],
+)]
+#[Groups(['book:read'])]
 class Book
 {
     #[ORM\Id]
@@ -17,17 +34,26 @@ class Book
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['book:write'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['book:write'])]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['book:write'])]
     private ?string $text = null;
 
     #[ORM\ManyToOne(inversedBy: 'books')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['book:write'])]
     private ?Category $cetegory = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['book:write'])]
+    private ?MediaObject $image = null;
 
     public function getId(): ?int
     {
@@ -78,6 +104,18 @@ class Book
     public function setCetegory(?Category $cetegory): static
     {
         $this->cetegory = $cetegory;
+
+        return $this;
+    }
+
+    public function getImage(): ?MediaObject
+    {
+        return $this->image;
+    }
+
+    public function setImage(MediaObject $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
